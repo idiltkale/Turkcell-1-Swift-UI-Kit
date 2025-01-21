abd5028bbe7226c3b999cd5cfa9e991fd9a2ce37
idil toprakale <idiltkale@gmail.com>
2025-01-21 14:42:33 +0300

    Initial Commit

---
+++ Product Project UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Product Project UIKit
+//
+//  Created by İdil Toprakkale on 21.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
